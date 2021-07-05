192e4a277c2701d498cf592d1da9ebd05673140e
AnastasiiaBos <kholina.a@gmail.com>
2021-07-06 01:37:33 +0200

    финальная версия

--- js/connection.js
+++ js/connection.js
@@ -16,7 +16,7 @@ try {
 
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  popup.classList.add("modal-connection-show");
   if (storage) {
     login.value = storage;
     email.focus();
@@ -28,6 +28,9 @@ link.addEventListener("click", function(evt) {
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value) {
     evt.preventDefault();
+    popup.classList.remove("modal-connection-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-connection-error");
   } else {
     if (isStorageSupport) {
       localStorage.setItem("login", login.value);
@@ -37,14 +40,16 @@ form.addEventListener("submit", function (evt) {
 
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-connection-show");
+  popup.classList.remove("modal-connection-error");
 });
 
 window.addEventListener("keydown", function(evt) {
   if (evt.key==="Esc"||evt.key==="Escape") {
-    if (popup.classList.contains("modal-show")) {
+    if (popup.classList.contains("modal-connection-show")) {
       evt.preventDefault();
-      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-connection-show");
+      popup.classList.remove("modal-connection-error");
     }
   }
 });
